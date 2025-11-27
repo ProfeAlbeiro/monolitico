aad0834493b19da92f02c48d58fb680b64e7d485
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-11-27 15:10:29 -0500

    prueba local - externo

--- models/DataBase.php
+++ models/DataBase.php
@@ -1,32 +1,32 @@
 <?php
     class DataBase{
         #  Conexión Local
-        // public static function connection(){
-        //     $hostname = "localhost";
-        //     $port = "3306";
-        //     $database = "database_php";
-        //     $username = "root";
-        //     $password = "";
-		// 	$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
-		// 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// 	return $pdo;
-		// }
-        
-        ## Conexión Azure
         public static function connection(){
-            $hostname = "server-mono1.mysql.database.azure.com";
+            $hostname = "localhost";
             $port = "3306";
             $database = "database_php";
-            $username = "admin_mono1";
-            $password = "ConnMono.";
-            // $password = getenv('DB_PASSWORD');
-            $options = array(
-                PDO::MYSQL_ATTR_SSL_CA => 'assets/database/DigiCertGlobalRootG2.crt.pem'
-            );
-            $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        }
+            $username = "root";
+            $password = "";
+			$pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo;
+		}
+        
+        ## Conexión Azure
+        // public static function connection(){
+        //     $hostname = "server-mono1.mysql.database.azure.com";
+        //     $port = "3306";
+        //     $database = "database_php";
+        //     $username = "admin_mono1";
+        //     $password = "ConnMono.";
+        //     // $password = getenv('DB_PASSWORD');
+        //     $options = array(
+        //         PDO::MYSQL_ATTR_SSL_CA => 'assets/database/DigiCertGlobalRootG2.crt.pem'
+        //     );
+        //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password,$options);
+        //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //     return $pdo;
+        // }
         
 	}
 ?>
